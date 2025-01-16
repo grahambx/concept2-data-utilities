2cc854caf8375d697c52bedd091481313c8d3108
grahambx <graham.bance@gmail.com>
2025-01-16 20:01:38 +0000

    Added Data processing to save json to csv+xlsx and create weekly report

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+import os
 from src.api_client import APIClient
+from src.data_processor import RowingDataProcessor
 
 
 def main():
@@ -6,12 +8,25 @@ def main():
     Entry point for the application.
     - Initializes the API client.
     - Fetches all logbook data from the Concept2 API.
+    - Process data to convert to csv + xslx (have not unpacked nested lap data)
+    - Process data to generate a weekly report and save as csv + xlsx
     """
     # Initialise API Client
     api_client = APIClient()
 
     # Request logbook results (handles pagination and token management)
     api_client.fetch_logbook_data_all()
+
+    # Complete Data processing
+    input_folder = "output"
+    output_folder = "output"
+    print("\nAttempting to process data...")
+    processor = RowingDataProcessor(input_folder, output_folder)
+    processor.process()
+
+    # Open Aggregate weekly summary xlsx
+    # TODO - below will only work for Windows
+    os.startfile(os.path.join(output_folder, 'weekly-report.xlsx'))
 
 
 if __name__ == "__main__":
